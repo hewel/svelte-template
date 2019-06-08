e181e7a7ec56a039d02038a7b1f7cfa604255114
hewel <hewelzei@qq.com>
2019-06-08 16:49:25 +0800

    Template init

---
+++ src/App/App.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<style>
+    h1 {
+        font-size: 16px;
+        font-weight: 500;
+        color: rebeccapurple;
+    }
+</style>
+
+<h1>Hello Svelte!</h1>
+
+<button>Svelte</button>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import App from './App/App.svelte'
+import './css/global.css'
+
+const app = new App({
+    target: document.getElementById('root'),
+})
+
+// window.app = app
+
+export default app
